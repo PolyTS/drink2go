461482924356713f4f28e39a351b046489a457f0
PolyTS <p.sanfirova@mail.ru>
2023-06-30 18:00:16 +0300

    index.html+style

---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+/* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.navigation');
+let navToggle = document.querySelector('.navigation__toggle');
+
+navMain.classList.remove('navigation--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('navigation--close')) {
+    navMain.classList.remove('navigation--close');
+    navMain.classList.add('navigation--open');
+  } else {
+    navMain.classList.add('navigation--close');
+    navMain.classList.remove('navigation--open');
+  }
+});
