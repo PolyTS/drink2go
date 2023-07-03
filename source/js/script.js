eb56b0d067a68083418d5b46aeba558f8c241a71
PolyTS <p.sanfirova@mail.ru>
2023-07-03 15:29:20 +0300

    pixel perfect

--- source/js/script.js
+++ source/js/script.js
@@ -13,3 +13,23 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('navigation--open');
   }
 });
+
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
